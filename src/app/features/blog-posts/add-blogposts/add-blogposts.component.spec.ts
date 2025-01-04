1cd892896ce345506d89fe522c6352d52d1d2743
basel68 <baselfarouk09@gmail.com>
2025-01-05 00:35:15 +0200

    blogpost components unit tests

--- src/app/features/blog-posts/add-blogposts/add-blogposts.component.spec.ts
+++ src/app/features/blog-posts/add-blogposts/add-blogposts.component.spec.ts
@@ -1,23 +1,88 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ImageSelectorService } from './../../../shared/components/image-selector/image-selector.service';
+import { ComponentFixture, fakeAsync, flush, TestBed } from '@angular/core/testing';
 import { AddBlogpostsComponent } from './add-blogposts.component';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { MarkdownModule, MarkdownService } from 'ngx-markdown';
+import { of } from 'rxjs';
+import { MOCK_BLOG_POSTS, MOCK_CATEGORIES, MOCK_IMAGES } from '../../../db-mock';
+import { BlogpostService } from '../services/blogpost.service';
+import { CategoryService } from '../../categories/services/category.service';
 
-describe('AddBlogpostsComponent', () => {
+fdescribe('AddBlogpostsComponent', () => {
   let component: AddBlogpostsComponent;
   let fixture: ComponentFixture<AddBlogpostsComponent>;
-
+  let blogpostServiceSpy: jasmine.SpyObj<BlogpostService>;
+  let categoryServiceSpy: jasmine.SpyObj<CategoryService>;
+  let imageselectorServiceSpy:jasmine.SpyObj<ImageSelectorService>;
+  let routerSpy: jasmine.SpyObj<Router>;
   beforeEach(async () => {
+  blogpostServiceSpy= jasmine.createSpyObj('BlogpostService', ['getAllBlogPosts','addBlogPost']);
+  categoryServiceSpy= jasmine.createSpyObj('CategoryService', ['getAllCategories']);
+  imageselectorServiceSpy=jasmine.createSpyObj('ImageSelectorService', ['selectImageService','getAllImages']);
+  routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+
+  blogpostServiceSpy.getAllBlogPosts.and.returnValue(of(MOCK_BLOG_POSTS));
+  categoryServiceSpy.getAllCategories.and.returnValue(of(MOCK_CATEGORIES));
+  imageselectorServiceSpy.selectImageService.and.returnValue(of(MOCK_IMAGES[0]));
+  imageselectorServiceSpy.getAllImages.and.returnValue(of(MOCK_IMAGES));
+
     await TestBed.configureTestingModule({
-      imports: [AddBlogpostsComponent]
+      imports: [
+        AddBlogpostsComponent,
+        RouterModule.forRoot([]),
+        CommonModule,
+        MarkdownModule.forRoot()
+      ],
+      providers:[
+        provideHttpClient(withInterceptorsFromDi()),
+        { provide: BlogpostService, useValue: blogpostServiceSpy },
+        { provide: CategoryService, useValue: categoryServiceSpy },
+        { provide: ImageSelectorService, useValue: imageselectorServiceSpy },
+        { provide: Router, useValue: routerSpy }
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(AddBlogpostsComponent);
     component = fixture.componentInstance;
+    blogpostServiceSpy=TestBed.inject(BlogpostService) as jasmine.SpyObj<BlogpostService>
+    categoryServiceSpy=TestBed.inject(CategoryService) as jasmine.SpyObj<CategoryService>
+    imageselectorServiceSpy=TestBed.inject(ImageSelectorService) as jasmine.SpyObj<ImageSelectorService>
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create add blog post component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display all available categories ', fakeAsync(() => {
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(categoryServiceSpy.getAllCategories).toHaveBeenCalled();
+    component.categories$?.subscribe(categories=>{
+      expect(categories).toBe(MOCK_CATEGORIES);
+    });
+    flush();
+  }));
+  it('should display image selected and image modal closed',() => {
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(imageselectorServiceSpy.selectImageService).toHaveBeenCalled();
+    expect(component.model.featuredImageUrl).toBe(MOCK_IMAGES[0].url);
+    expect(component.isImageSelectorVisible).toBeFalsy();
+  });
+
+  it('should navigate to /admin/blogposts after successful form submission', () => {
+    // Arrange: Mock the BlogpostService to simulate successful submission
+    blogpostServiceSpy.addBlogPost.and.returnValue(of(MOCK_BLOG_POSTS[0]));
+
+    // Act: Call the onFormSubmit method
+    component.onFormSubmit();
+
+    // flush();
+    // Assert: Check if navigateByUrl was called with the correct URL
+    expect(routerSpy.navigateByUrl).toHaveBeenCalledWith('/admin/blogposts');
   });
 });
--- src/app/features/blog-posts/blogpost-list/blogpost-list.component.spec.ts
+++ src/app/features/blog-posts/blogpost-list/blogpost-list.component.spec.ts
@@ -1,23 +1,87 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BlogpostListComponent } from './blogpost-list.component';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MOCK_BLOG_POSTS } from '../../../db-mock';
+import { of } from 'rxjs';
+import { BlogpostService } from '../services/blogpost.service';
+import { By } from '@angular/platform-browser';
 
 describe('BlogpostListComponent', () => {
   let component: BlogpostListComponent;
   let fixture: ComponentFixture<BlogpostListComponent>;
+  let blogpostService: jasmine.SpyObj<BlogpostService>;
 
   beforeEach(async () => {
+  const blogpostServiceSpy= jasmine.createSpyObj('BlogpostService', ['getAllBlogPosts']);
+  blogpostServiceSpy.getAllBlogPosts.and.returnValue(of(MOCK_BLOG_POSTS))
     await TestBed.configureTestingModule({
-      imports: [BlogpostListComponent]
+      declarations:[],
+      imports: [
+        RouterModule.forRoot([]),
+        CommonModule,
+        BlogpostListComponent
+      ],
+      providers:[
+        provideHttpClient(withInterceptorsFromDi()),
+        {provide:BlogpostService,useValue:blogpostServiceSpy}
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(BlogpostListComponent);
     component = fixture.componentInstance;
+    blogpostService=TestBed.inject(BlogpostService) as jasmine.SpyObj<BlogpostService>
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create blog post list component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load blogposts on init', () => {
+    component.ngOnInit();
+
+    fixture.detectChanges();
+
+    expect(blogpostService.getAllBlogPosts).toHaveBeenCalled();
+
+    const tableRows=fixture.nativeElement.querySelectorAll("tbody tr");
+
+    expect(tableRows.length).toBe(MOCK_BLOG_POSTS.length);
+  });
+  it('should render categories as badges ', () => {
+    component.ngOnInit();
+
+    fixture.detectChanges();
+
+    expect(blogpostService.getAllBlogPosts).toHaveBeenCalled();
+
+    const badges=fixture.nativeElement.querySelectorAll(".badge");
+
+    expect(badges.length).toBe(MOCK_BLOG_POSTS.reduce((acc,post)=>acc+post.categories.length,0));
+  });
+
+  it('should assign blog posts to blogPosts observable', (done) => {
+
+    component.ngOnInit();
+
+    component.blogPosts$?.subscribe(posts => {
+      expect(posts).toEqual(MOCK_BLOG_POSTS);
+      expect(posts.length).toBe(3);
+      done();
+    });
+  });
+  it('should have correct router link for Add BlogPost button', () => {
+    const addButton = fixture.debugElement.query(By.css('a.btn-primary'));
+    const routerLink = addButton.attributes['ng-reflect-router-link'];
+    expect(routerLink).toBe('/admin/blogposts/add');
+  });
+
+  it('should have correct router link for Edit BlogPost button', () => {
+    const addButton = fixture.debugElement.queryAll(By.css('a.btn-light'));
+    const routerLink = addButton[0].attributes['ng-reflect-router-link'];
+    expect(routerLink).toBe(`/admin/blogposts,${MOCK_BLOG_POSTS[0].id}`);
   });
 });
